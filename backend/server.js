0eb48598f36fc62690245f905d97e7e7d2d77af9
unknown <zaaferkenza08@gmail.com>
2023-04-06 07:21:44 +0000

    Routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,30 @@ const express = require('express');
 
 const app = express();
 
+//Routes
+//Users Routes
+app.post('/api/users/register' , (req,res) => {
+    res.send('Register Route');
+});
+
+app.post('/api/users/login' , (req,res) => {
+    res.send('Login Route');
+});
+
+app.put('/api/users/update' , (req,res) => {
+    res.send('Update Route');
+});
+
+app.delete('/api/users/:id' , (req,res) => {
+    res.send('Delete Route');
+});
+
+//Fetch Users
+app.get('/api/users/' , (req,res) => {
+    res.send('Fetch users');
+})
+
+//server
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,() => {
