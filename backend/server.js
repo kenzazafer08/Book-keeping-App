901e88b54529c55280740200f44baa20cdfb9d2f
unknown <zaaferkenza08@gmail.com>
2023-04-06 07:57:21 +0000

    Connecting to Mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+//connect db
+mongoose.connect('mongodb+srv://kanzazafer:kenzazafer08@book-app.iigock6.mongodb.net/book-keeping-app',{
+    useUnifiedTopology : true,
+    useNewUrlParser : true
+}).then(() => console.log('DB connected')).catch(err => console.log(err));
+
 
 //Routes
 //Users Routes
